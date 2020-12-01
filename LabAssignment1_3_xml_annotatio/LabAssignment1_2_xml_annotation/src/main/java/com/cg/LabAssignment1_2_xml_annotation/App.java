7055d305b2fef1d0fc9ed6d8871b52c8267f89fa
ANUSHAR <anusharkowndinya71@gmail.com>
2020-12-01 21:28:24 +0530

    done

---
+++ LabAssignment1_3_xml_annotatio/LabAssignment1_2_xml_annotation/src/main/java/com/cg/LabAssignment1_2_xml_annotation/App.java
@@ -0,0 +1,16 @@
+package com.cg.LabAssignment1_2_xml_annotation;
+	import org.springframework.context.ApplicationContext;
+	import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+	public class App 
+	{
+	    public static void main( String[] args )
+	    {
+	    	ApplicationContext ctx=new ClassPathXmlApplicationContext("bean.xml");
+			Employee emp=(Employee)ctx.getBean("employee");
+			System.out.println(emp);
+	    }
+	}
+
+
---
+++ LabAssignment1_3_xml_annotatio/LabAssignment1_2_xml_annotation/src/main/java/com/cg/LabAssignment1_2_xml_annotation/LabAssignment12XmlAnnotationApplication.java
@@ -0,0 +1,13 @@
+package com.cg.LabAssignment1_2_xml_annotation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabAssignment12XmlAnnotationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabAssignment12XmlAnnotationApplication.class, args);
+	}
+
+}
